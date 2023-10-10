4dc77458e7a68e11e54dd63231881cee862c7235
Hadirman Gea <hadirmangea25@gmail.com>
2023-10-10 13:39:12 +0700

    up frot

--- js/track.js
+++ js/track.js
@@ -17,7 +17,13 @@ document.addEventListener("DOMContentLoaded", function () {
 
       if (response.ok) {
         const result = await response.json();
-        daftarfromtracker.innerHTML = JSON.stringify(result);
+        daftarfromtracker.innerHTML = `<p>Receivers Name: ${result.ReceiversName}, <br /> 
+              Tracking Number: ${result.TrackingNumber}, <br />
+              Phone Number: ${result.PhoneNumber}, <br />
+              Package Weight: ${result.PackageWeight}, <br />
+              Service Option: ${result.ServiceOption} <br />
+          </p>
+          <h1>Success</h1>`;
         daftarfromtracker.appendChild(formTrack);
         
       } else {
